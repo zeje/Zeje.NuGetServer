b1fe700735185e35ddc51ca86012ebc1be34f9ac
zeje <zezhi821@163.com>
2016-08-09 12:47:15 +0800

    自建NuGet服务器站点

---
+++ Zeje.NuGetServer/Global.asax.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Zeje.NuGetServer
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            if (Context.Request.FilePath == "/")
+            {
+                Context.RewritePath("~/Default.aspx");
+            }
+        }
+    }
+}
